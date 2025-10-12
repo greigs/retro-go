9efcb56ac52ba9fc50c17f7297ee939b36d65db0
greigs <>
2025-10-12 12:44:07 +0100

    undo change to prboom

--- prboom-go/components/prboom/m_misc.c
+++ prboom-go/components/prboom/m_misc.c
@@ -262,8 +262,8 @@ static default_t defaults[] =
   {"pitched_sounds",{&pitched_sounds},{0},0,1, // killough 2/21/98
    def_bool,ss_none}, // enables variable pitch in sound effects (from id's original code)
   {"samplerate",{&snd_samplerate},{22050},11025,48000, def_int,ss_none},
-  {"sfx_volume",{&snd_SfxVolume},{15},0,15, def_int,ss_none},
-  {"music_volume",{&snd_MusicVolume},{15},0,15, def_int,ss_none},
+  {"sfx_volume",{&snd_SfxVolume},{8},0,15, def_int,ss_none},
+  {"music_volume",{&snd_MusicVolume},{8},0,15, def_int,ss_none},
   {"mus_pause_opt",{&mus_pause_opt},{2},0,2, // CPhipps - music pausing
    def_int, ss_none}, // 0 = kill music when paused, 1 = pause music, 2 = let music continue
   {"snd_channels",{&snd_channels},{8},1,32,
